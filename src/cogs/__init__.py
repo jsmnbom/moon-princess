605cd34f8bfff9eda0aac147926917c43a630807
Jasmin Bom <jsmnbom@gmail.com>
2019-10-11 13:11:19 +0200

    Add global error handler

--- src/cogs/__init__.py
+++ src/cogs/__init__.py
@@ -1,4 +1,5 @@
 from .emotes import Emotes
 from .dadbot import DadBot
+from .error import CommandErrorHandler
 
-__all__ = ['Emotes', 'DadBot']
+__all__ = ['Emotes', 'DadBot', 'CommandErrorHandler']
